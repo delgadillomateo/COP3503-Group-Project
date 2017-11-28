b363c96a29fb4f8094a28d6dcc5e6b1daa3b7dc3
MarcoKalnay <33135525+MarcoKalnay@users.noreply.github.com>
2017-11-28 15:07:26 -0500

    Add files via upload

---
+++ PlayerClass.cpp
@@ -0,0 +1,56 @@
+// GroupProj.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class PlayerClass
+{
+private:
+	string name;
+	int score;
+	PlayerClass(string, int);
+
+public:
+	void setName(string);
+	string getName();
+	void printName();
+	void addScore(int);
+	int getScore(int);
+	void printScore();
+};
+
+void PlayerClass::setName(string tempN)
+{
+	name = tempN;
+}
+
+string PlayerClass::getName()
+{
+	return name;
+}
+
+void PlayerClass::printName()
+{
+	cout << name << endl;
+}
+
+void PlayerClass::addScore(int tempS)
+{
+	score = tempS + score;
+}
+
+int PlayerClass::getScore(int tempS)
+{
+	return score;
+}
+
+void PlayerClass::printScore()
+{
+	cout << name << " score is " << score;
+}
+
+
